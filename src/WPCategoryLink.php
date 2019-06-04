cadc4b1445893eb926104f9f765f9ca75fbb7e4f
Jaimeson Waugh <jaimeson@4cesi.com>
2019-06-04 13:58:55 -0700

    Update TestHashItem dependency

--- src/WPCategoryLink.php
+++ src/WPCategoryLink.php
@@ -1,45 +1,43 @@
 <?php
 
 declare( strict_types = 1 );
-namespace WaughJ\WPCategoryLink
+namespace WaughJ\WPCategoryLink;
+
+use \WaughJ\HTMLLink\HTMLLink;
+use WaughJ\TestHashItem\TestHashItem;
+
+class WPCategoryLink extends HTMLLink
 {
-	use \WaughJ\HTMLLink\HTMLLink;
-	use function WaughJ\TestHashItem\TestHashItemExists;
-	use function WaughJ\TestHashItem\TestHashItemString;
-
-	class WPCategoryLink extends HTMLLink
+	public function __construct( array $atts )
 	{
-		public function __construct( array $atts )
+		$category = self::GetCategoryID( $atts );
+		if ( $category !== null )
 		{
-			$category = self::GetCategoryID( $atts );
-			if ( $category !== null )
-			{
-				$atts[ 'href' ] = get_category_link( $category->term_id );
-				$atts[ 'value' ] = TestHashItemExists( $atts, 'value', $category->title );
-			}
-			else
-			{
-				$atts[ 'href' ] = TestHashItemExists( $atts, 'href', TestHashItemString( $atts, 'slug', '' ) );
-				$atts[ 'value' ] = TestHashItemExists( $atts, 'value', $atts[ 'href' ] );
-			}
-
-			$href = $atts[ 'href' ];
-			$value = $atts[ 'value' ];
-			parent::__construct( $href, $value, $atts );
+			$atts[ 'href' ] = get_category_link( $category->term_id );
+			$atts[ 'value' ] = $atts[ 'value' ] ?? $category->title;
+		}
+		else
+		{
+			$atts[ 'href' ] = $atts[ 'href' ] ?? TestHashItem::getString( $atts, 'slug', '' );
+			$atts[ 'value' ] = $atts[ 'value' ] ?? $atts[ 'href' ];
 		}
 
-		private static function GetCategoryID( array $atts )
+		$href = $atts[ 'href' ];
+		$value = $atts[ 'value' ];
+		parent::__construct( $href, $value, $atts );
+	}
+
+	private static function GetCategoryID( array $atts )
+	{
+		$category = false;
+		if ( isset( $atts[ 'slug' ] ) )
 		{
-			$category = false;
-			if ( isset( $atts[ 'slug' ] ) )
-			{
-				$category = get_category_by_slug( $atts[ 'slug' ] );
-			}
-			else if ( isset( $atts[ 'category_id' ] ) )
-			{
-				$category = get_category( $atts[ 'category_id' ] );
-			}
-			return ( $category === false ) ? null : $category;
+			$category = get_category_by_slug( $atts[ 'slug' ] );
 		}
+		else if ( isset( $atts[ 'category_id' ] ) )
+		{
+			$category = get_category( $atts[ 'category_id' ] );
+		}
+		return ( $category === false ) ? null : $category;
 	}
 }
